e7b5b29459d6968e844c262f836a544a94bf5ffd
didoloan <40449097+didoloan@users.noreply.github.com>
2024-12-23 22:54:27 +0100

    Update rust_prep.sh
    Signed-off-by: didoloan <40449097+didoloan@users.noreply.github.com>

--- rust_prep.sh
+++ rust_prep.sh
@@ -2,6 +2,7 @@ curl --proto '=https' --tlsv1.2 -sSf https://get.kraftkit.sh | sh
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
-rustup toolchain install nightly
-
 rustup default nightly
+
+rustup update nightly
+rustup component add rust-src --toolchain nightly-x86_64-unknown-linux-gnu
